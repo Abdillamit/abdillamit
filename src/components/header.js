39bd63d99798f4d069f0b56217d1cc9409d56854
Abdillamit Developer <developer@example.com>
2025-06-21 23:05:47 +0600

    refactor: simplify site layout by removing unnecessary components and styling

--- src/components/header.js
+++ src/components/header.js
@@ -1,25 +1,15 @@
+// External dependencies
 import * as React from "react"
-import { Link } from "gatsby"
 
-const Header = ({ siteTitle }) => (
+const Header = () => (
   <header
     style={{
-      margin: `0 auto`,
       padding: `var(--space-4) var(--size-gutter)`,
       display: `flex`,
       alignItems: `center`,
       justifyContent: `space-between`,
     }}
   >
-    <Link
-      to="/"
-      style={{
-        fontSize: `var(--font-sm)`,
-        textDecoration: `none`,
-      }}
-    >
-      {siteTitle}
-    </Link>
     <img
       alt="Gatsby logo"
       height={20}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,26 +1,13 @@
+// External dependencies
 import * as React from "react"
+
+// Local dependencies
 import Layout from "../components/layout"
 import Seo from "../components/seo"
-import EnvironmentInfo from "../components/environment-info"
 
 const IndexPage = () => (
   <Layout>
-    <div style={{ 
-      textAlign: 'center', 
-      padding: '2rem',
-      fontSize: '2rem',
-      fontWeight: 'bold',
-      color: '#663399'
-    }}>
-      <h1>Hello Abdillamit</h1>
-      <p style={{ fontSize: '1.2rem', fontWeight: 'normal', marginTop: '1rem' }}>
-        Welcome to your Gatsby.js site deployed on AWS!
-      </p>
-    </div>
-    
-    <div style={{ maxWidth: '600px', margin: '0 auto', padding: '0 1rem' }}>
-      <EnvironmentInfo />
-    </div>
+    <h1>Hello Abdillamit</h1>
   </Layout>
 )
 
